ff4f81fb56d52f42c3587b7ccf542e1fa57818e3
Ethan <Ethan@157.160.79.43>
2015-02-16 13:41:00 -0500

    Added functionality to show requests from browser

--- CSC431Project1/src/WebServer.java
+++ CSC431Project1/src/WebServer.java
@@ -1,8 +1,68 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
-public class WebServer {
+public final class WebServer {
 
-	public static void main(String[] args) {
-
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws Exception{
+		//Set the port number.
+		int port = 6789;
+		ServerSocket serverSocket = null;
+		//Establish the listen socket.
+		try{
+			serverSocket = new ServerSocket(port);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		//Process HTTP service requests in an infinite loop.
+		while(true){
+			Socket socket = serverSocket.accept();
+			HttpRequest request = new HttpRequest(socket);
+			Thread thread = new Thread(request);
+			thread.start();
+		}
 	}
 
+}
+final class HttpRequest implements Runnable{
+	final static String CRLF = "\r\n";
+	Socket socket;
+	
+	/**
+	 * Constructor for an http request, binding a socket.
+	 * @param socket: the socket to be binded for this request
+	 */
+	public HttpRequest(Socket socket) {
+		this.socket = socket;
+	}
+
+	@Override
+	public void run() {
+		try{
+			processRequest();
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
+	
+	private void processRequest() throws Exception{
+		InputStream is = socket.getInputStream();
+		DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		
+		String requestLine = br.readLine();
+		System.out.println();
+		System.out.println(requestLine);
+		
+		String headerLine = null;
+		while((headerLine = br.readLine()).length() != 0 ){
+			System.out.println(headerLine);
+		}
+		os.close();
+		br.close();
+		socket.close();
+	}
+	
 }
